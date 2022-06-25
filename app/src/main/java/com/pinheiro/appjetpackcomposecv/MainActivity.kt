8084564eccf544a2c517ab5ce6b89a063b96011d
Pinheiro <mateuspinheirodev@gmail.com>
2022-06-25 01:40:38 -0300

    1.1 - Adicionadas mais informações e a re-organização das mesas fora efetivada. Também fora inclusa a imagem de perfi

--- app/src/main/java/com/pinheiro/appjetpackcomposecv/MainActivity.kt
+++ app/src/main/java/com/pinheiro/appjetpackcomposecv/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.pinheiro.appjetpackcomposecv
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +14,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import com.pinheiro.appjetpackcomposecv.ui.theme.AppJetpackComposeCvTheme
 
@@ -34,16 +38,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AuthorInformation() {
-    Row(verticalAlignment = Alignment.Top) {
-        Column {
+        Row(verticalAlignment = Alignment.Top) {
+                Column {
+                    Row(verticalAlignment = Alignment.Top) {
+                        Image(
+                            painter = painterResource(id = R.drawable.perfil),
+                            contentDescription = "Author"
+                        )
+                    }
             Text("Mateus Pinheiro")
             Text("Dev Android Jr.")
-            Text("Sample of Jetpack Compose app")
+            Text("GitHub: mateeuspinheiro")
         }
         Column {
             Text("Graduate Systems Analyst")
             Text("Extensive experiencie with databases {Oracle / Postgres / Firebird / MySql / Sqlite")
             Text("+8 years of experience working as mobile development team supervisor and deployment team")
+
+
         }
     }
 }
